736d18c3f940ba40e11c36c29ea2a3a8ae11d0da
Loai Dev <loaidev64@gmail.com>
2023-07-10 10:38:48 +0300

    dsakdosak

--- lib/ui/widegets/drop.dart
+++ lib/ui/widegets/drop.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_2/ui/Screens/controllers/Martket1Controller.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 import '../Screens/controllers/Market3Controller.dart';
@@ -22,7 +23,7 @@ class _MyWidgetState extends State<MyWidget> {
   void initState() {
     super.initState();
 
-    TrendingController.getcurrency().then((value) {
+    WatichlistController.getcurrency().then((value) {
       setState(() {
         currency = value;
       });
